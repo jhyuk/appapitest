b2ed13b6390e95cc0668975a1a040a1c3af1c465
jhyuk <akajhyuk@gmail.com>
2018-05-23 16:19:01 +0900

    first commit

---
+++ post/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
